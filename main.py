cc6865de5dfbf771fa21d18443721d15554a0751
Thibault Biville <thibault.biville@gmail.com>
2024-02-06 15:42:24 +0100

    model1

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from models.model1 import model1
+#from models.model2 import model2
+#etc...
+from routes import api
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-    
-app.run(host="0.0.0.0")
+app.register_blueprint(api)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ routes/api.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, request, jsonify
+from models.model1 import model1
+#from models.model2 import model2
+#etc...
+import pandas as pd
+
+api = Blueprint('api', __name__)
+
+@api.route('/predict', methods=['GET'])
+def predict():
+
+    parameters = request.args.to_dict()
+
+    prediction1 = model1.predict(pd.DataFrame(parameters, index=[0]))
+    # Prediction for Model 2
+    # ...
+
+
+    #consensus_prediction = (prediction1 + prediction2) / 2
+
+
+    response = {
+        'consensus_prediction': consensus_prediction.tolist(),
+        'individual_predictions': {
+            'model1': prediction1.tolist(),
+            'model2': prediction2.tolist(),
+            # ...
+        }
+    }
+
+    return jsonify(response)
