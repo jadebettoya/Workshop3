4ce7c82d711516a8fb1b06697ddf43355f341e1a
jadebettoya <104717442+jadebettoya@users.noreply.github.com>
2024-02-06 15:05:26 +0000

    q2

--- routes/api.py
+++ routes/api.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from models.model1 import model1
-#from models.model2 import model2
+from models.model2 import model2
 #etc...
 import pandas as pd
 
@@ -12,11 +12,12 @@ def predict():
     parameters = request.args.to_dict()
 
     prediction1 = model1.predict(pd.DataFrame(parameters, index=[0]))
+    prediction2 = model2.predict(pd.DataFrame(parameters, index=[0]))
     # Prediction for Model 2
     # ...
 
 
-    #consensus_prediction = (prediction1 + prediction2) / 2
+    consensus_prediction = (prediction1 + prediction2) / 2
 
 
     response = {
