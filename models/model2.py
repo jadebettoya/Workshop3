9bebc8bc580313899e4eb7a4b7790a49bcfcc10c
jackBlaster <80394391+jackBlaster@users.noreply.github.com>
2024-02-26 16:25:51 +0100

    new version
    working version of the api with models 1, 2, and 3

---
+++ models/model2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_iris
+
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+y_train = y_train.ravel()
+
+model2 = SVC(kernel='linear', random_state=42)
+model2.fit(X_train, y_train)
+
